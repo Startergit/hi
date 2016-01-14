cebc46ca8c9ac355fb1912fdad0fb0dfbbabb291
David Pan <codingisfun26@gmail.com>
2016-01-14 00:32:25 -0800

    resolved error after rake db:migrate. destroyed and recreated database

--- db/migrate/20160113210455_devise_create_users.rb
+++ db/migrate/20160113210455_devise_create_users.rb
@@ -2,8 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              default: ""
+      t.string :encrypted_password, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -31,7 +31,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
