0999c6f69c22dd5f19abea380aa50a7eb5af145c
David Pan <codingisfun26@gmail.com>
2016-01-21 00:23:59 -0800

    placed add_index :pins, :user_id underneath create_table :pins do |t|

--- db/migrate/20160114080849_create_pins.rb
+++ db/migrate/20160114080849_create_pins.rb
@@ -1,10 +1,10 @@
 class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
+    	add_index :pins, :user_id
       t.string :description
 
       t.timestamps
     end
-    add_index :pins, :user_id
   end
 end
